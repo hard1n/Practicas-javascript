a35675633123f93d2d24b4a85f5d8efdeb95b907
hard1n <hardisonpaulino@gmail.com>
2021-08-14 04:36:59 -0700

    formulario del tiángulo funcional

--- README.md
+++ README.md
@@ -7,3 +7,17 @@ Practicas de JavaScript
 - 2do paso: Implementar lar fórmulas en JavaScript
 - 3er paso: Crear funciones
 - 4tos paso: Integrar JS con HTML
+
+## Taller #2: pocertajes y descuentos
+
+- 1er paso: Definir las fórmulas
+- 2do paso: Implementar lar fórmulas en JavaScript
+- 3er paso: Crear funciones
+- 4tos paso: Integrar JS con HTML
+
+## Taller #3: promedio, mediana y moda
+
+- 1er paso: Definir las fórmulas
+- 2do paso: Implementar lar fórmulas en JavaScript
+- 3er paso: Crear funciones
+- 4tos paso: Integrar JS con HTML
--- figuras.js
+++ figuras.js
@@ -85,6 +85,146 @@ function calcularAreaCuadrado() {
     const value = input.value;
     const area = areaCuadrado(value);
     let resultadoCuadrado = document.getElementById("square-result");
-    
+
     resultadoCuadrado.textContent = "Area: " + area;
+}
+
+//Triangulo
+function calcularPerimetroTriangulo() {
+    const lado1 = Number(document.getElementById("triangle-input-side1").value);
+    const lado2 = Number(document.getElementById("triangle-input-side1").value);
+    const base= Number(document.getElementById("triangle-input-base").value);
+    const perimetro = perimetroTriangulo(lado1, lado2, base);
+    let resultadoTriangulo = document.getElementById("triangle-result");
+    
+    resultadoTriangulo.textContent = "Perimetro: " + perimetro;
+} 
+
+function calcularAreaTriangulo() {
+    const base = Number(document.getElementById("triangle-input-base").value);
+    const altura = Number(document.getElementById("triangle-input-height").value);
+    const area = areaTriangulo(base, altura);
+    let resultadoTriangulo = document.getElementById("triangle-result");
+
+    resultadoTriangulo.textContent = "Area: " + area;
+}
+
+function calcularAlturaTrianguloIsosceles(){
+    const lado1 = Number(document.getElementById("triangle-input-side1").value);
+    const lado2 = Number(document.getElementById("triangle-input-side2").value);
+    const base = Number(document.getElementById("triangle-input-base").value);
+    let resultadoTriangulo = document.getElementById("triangle-result");
+
+    if(lado1 === lado2 && lado1 != base){
+        const altura = Math.sqrt(lado1**2 - (base **2 / 4));;
+
+        resultadoTriangulo.textContent = "Altura (Triangulo isosceles): " + altura;
+    }
+    else{
+        resultadoTriangulo.textContent = "No es un triangulo isosceles";
+    }
+}
+/* --------------------Final del bloque geometría--------------------------------*/
+
+/********************Bloque Porcentajes y descuento*********************/
+const precioOriginal = 120;
+const descuento = 18;
+
+// console.log({
+//     precioOriginal,
+//     descuento,
+//     porcentajePrecioConDescuento,
+//     precioConDescuento
+// })
+
+function calcularPrecioConDescuento(precio, descuento) {
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+
+    return precioConDescuento;
+}
+
+function onClickPriceDiscount() {
+    const price = Number(document.getElementById("inputPrice").value);
+    const discount = Number(document.getElementById("inputDiscount").value);
+
+    const result = calcularPrecioConDescuento(price, discount);
+    const resultPrg = document.getElementById("resultPrice");
+    resultPrg.innerText = "El precio con descuento es: $" + result;
+
+    // validation1 = document.getElementById("input-price").reportValidity();
+    // validation1 = document.getElementById("input-discount").reportValidity();
+    // if(validation1 === true && validation2 === true){ // el true dice que el usuario pasó los parámetros que le pusimos, después si puede recibir el precio final. (también puede ser false si falló en algo)
+    //     const inputPriceValue = document.getElementById("input-price").value;
+    //     const inputDiscountValue = document.getElementById("input-discount").value;
+    //     const offPriceParsed = parseFloat(calculateOffPrice(inputPriceValue, inputDiscountValue).toFixed(2));
+    // } 
+}
+
+/* primera manera */
+// const coupons = [
+//     "super",
+//     "especial",
+//     "expert",
+// ];
+
+/* Cupón y valor en el mismo lugar */
+const coupons = [
+    {
+        name: "super",
+        discount: 15,
+    },
+    {
+        name: "especial",
+        discount: 30,
+    },
+    {
+        name: "expert",
+        discount: 25,
+    },
+];
+
+function onClickPriceCoupon() {
+    const price = Number(document.getElementById("inputPriceCoupon").value);
+    const coupon = document.getElementById("inputCoupon").name;
+
+    // let discount;
+
+    if (!coupons) {
+        alert("El cupón " + coupon + " no es válido");
+    } else {
+        const discount = coupons.discount;
+        
+        const result = calcularPrecioConDescuento(price, discount);
+        const resultPrg = document.getElementById("resultPrice");
+        resultPrg.innerText = "El precio con descuento es: $" + result;
+    }
+    /*
+    if (!coupons.includes(coupon)) {
+        alert("El cupón " + coupon + " no es válido");
+        discount = "Cupón inválido"
+    }
+    else if (coupon === "super") {
+        discount = 15;
+     } else if (coupon === "especial") {
+        discount = 30;
+     } else if (coupon === "expert") {
+        discount = 25;
+     }*/
+
+    /*switch (coupon) {
+        case "super":
+            discount = 15;
+        break;
+        case "especial":
+            discount = 20;
+        break;
+        case "expert":
+            discount = 25;
+        break;
+    }*/
+
+    /*const result = calcularPrecioConDescuento(price, discount);
+    const resultPrg = document.getElementById("resultPrice");
+    resultPrg.innerText = "El precio con descuento es: $" + result;*/
 }
